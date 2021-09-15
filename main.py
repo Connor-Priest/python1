1039546a4bc57ac915c61d1aa5884d67cbc59515
Connor-Priest <cpriest965@g.rwu.edu>
2021-09-15 13:29:56 -0400

    Ask and multiply number

--- main.py
+++ main.py
@@ -9,6 +9,14 @@ def ask_name():
     #print the users name
     print(f'Hello {name}')
 
+def number_multiply():
+    #ask the user for a number
+    number = int(input("Enter a number :"))
+    #multiply the users number by itself
+    number_multiplied = number * number
+    print("Your multiplied number is: ",number_multiplied)
+
+
 def print_hi(name):
     # Use a breakpoint in the code line below to debug your script.
     print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
@@ -18,5 +26,6 @@ def print_hi(name):
 if __name__ == '__main__':
     #print_hi('Connor Priest')
     ask_name()
+    number_multiply()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
