f3c355ddfdea939a45203bc3b378a432cacb480d
Connor-Priest <cpriest965@g.rwu.edu>
2021-10-24 13:47:08 -0400

    added os to new branch

--- main.py
+++ main.py
@@ -2,8 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import random
+import os
 
 
-list1 = [1, 5, 5, 7, 1, 8, 4, 2, 7, 8, 3, 6, 4]
-print(random.choice(list1))
+cwd = os.getcwd()
+print("Your current working directory is:", cwd)
