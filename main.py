50f392fd20a97df14b74d1f02935fc32ccc939e4
Connor-Priest <cpriest965@g.rwu.edu>
2021-10-24 13:39:50 -0400

    added calendar to new branch

--- main.py
+++ main.py
@@ -2,73 +2,9 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import random
+import calendar
 
 
-def ask_name():
-    # ask the users name
-    name = input("What is your name :")
-    # print the users name
-    print(f'Hello {name}')
-
-
-def number_multiply():
-    # ask the user for a number
-    number = int(input("Enter a number :"))
-    # multiply the users number by itself
-    number_multiplied = number * number
-    print("Your multiplied number is: ", number_multiplied)
-
-
-def word_count():
-    # ask the user for a word
-    word = input("Please enter a word: ")
-    # count the letters in the word and print the number
-    print(len(word))
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-def loop():
-    array = [['.', '.', '.', '.', '.', '.', '.'],
-              ['.', '.', '.', '.', '.', '.', '.'],
-              ['.', 'O', '.', 'O', '0', '0', '.'],
-              ['.', '.', '.', '.', '.', '.', '.'],
-              ['.', 'O', 'O', 'O', '0', '0', '.'],
-              ['.', '.', '.', '0', '.', '.', '.'],
-              ['.', '.', '.', '0', '.', '.', '.'],
-              ['.', 'O', '0', '0', '0', '0', '.'],
-              ['.', '.', '.', '.', '.', '.', '.']]
-    print("Array: ")
-    for i in array:
-        for j in i:
-            print(j, end=" ")
-        print()
-
-
-def coin_flip():
-    number = int(input("Heads'(0)' or Tails'(1)': "))
-    flip = random.randint(0, 1)
-    if flip == 0:
-        print("Heads")
-    else:
-        print("Tails")
-    if number == flip:
-        print("You Win!")
-    else:
-        print("Sorry try again...")
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    # print_hi('Connor Priest')
-    # ask_name()
-    # number_multiply()
-    # word_count()
-    loop()
-    # coin_flip()
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+yy = 2021
+mm = 10
+print(calendar.month(yy, mm))
