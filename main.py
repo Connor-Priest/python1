77fb5b5eb3dec770c65b1d9e2c630dca3248ccb2
Connor-Priest <cpriest965@g.rwu.edu>
2021-10-24 13:43:51 -0400

    added random to new branch

--- main.py
+++ main.py
@@ -2,9 +2,8 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-import calendar
+import random
 
 
-yy = 2021
-mm = 10
-print(calendar.month(yy, mm))
+list1 = [1, 5, 5, 7, 1, 8, 4, 2, 7, 8, 3, 6, 4]
+print(random.choice(list1))
